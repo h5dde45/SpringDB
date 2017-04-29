95e6170239f183e836a2a70811c9025387d956fe
nov <nov@mail.ru>
2017-04-29 08:49:40 +0400

    insert

--- src/main/java/ru/javabegin/training/spring/dao/impls/SQLiteDAO.java
+++ src/main/java/ru/javabegin/training/spring/dao/impls/SQLiteDAO.java
@@ -5,8 +5,9 @@ import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 import ru.javabegin.training.spring.dao.interfaces.MP3Dao;
 import ru.javabegin.training.spring.dao.objects.Author;
@@ -22,139 +23,138 @@ import java.util.TreeMap;
 @Component("sqliteDAO")
 public class SQLiteDAO implements MP3Dao {
 
-	private static final String mp3Table = "mp3";
-	private static final String mp3View = "mp3_view";
+    private static final String mp3Table = "mp3";
+    private static final String mp3View = "mp3_view";
 
-	private SimpleJdbcInsert insertMP3;
+    private SimpleJdbcInsert insertMP3;
 
-	private NamedParameterJdbcTemplate jdbcTemplate;
+    private NamedParameterJdbcTemplate jdbcTemplate;
 
-	@Autowired
-	public void setDataSource(DataSource dataSource) {
-		this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
-		this.insertMP3 = new SimpleJdbcInsert(dataSource).withTableName("mp3").usingColumns("name", "author");
-	}
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+        this.insertMP3 = new SimpleJdbcInsert(dataSource).withTableName("mp3").usingColumns("name", "author");
+    }
 
-	@Override
-	public int insert(MP3 mp3) {
+    @Override
+    public int insert(MP3 mp3) {
+        String sqlInsAuth = "insert into author (name) values (:authorName)";
+        Author author = mp3.getAuthor();
 
-		MapSqlParameterSource params = new MapSqlParameterSource();
-		params.addValue("name", mp3.getName());
-		params.addValue("author", mp3.getAuthor());
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("authorName", author.getName());
+        KeyHolder keyHolder = new GeneratedKeyHolder();
 
-		return insertMP3.execute(params);
+        jdbcTemplate.update(sqlInsAuth, params, keyHolder);
+        int author_id = keyHolder.getKey().intValue();
 
-	}
+        String sqlInsMP3 = "insert into mp3 (author_id, name) values (:authorId, :mp3Name)";
 
-	@Override
-	public int insertList(List<MP3> listMP3) {
-		String sql = "insert into mp3 (name, author) VALUES ( :name,:author)";
+        params = new MapSqlParameterSource();
+        params.addValue("mp3Name", mp3.getName());
+        params.addValue("authorId", author_id);
 
-		SqlParameterSource[] params = new SqlParameterSource[listMP3.size()];
+        return jdbcTemplate.update(sqlInsMP3, params);
+    }
 
-		int i = 0;
+    @Override
+    public int insertList(List<MP3> listMP3) {
+       int i=0;
+       for(MP3 mp3:listMP3){
+           insert(mp3);
+           i++;
+       }
+       return i;
+    }
 
-		for (MP3 mp3 : listMP3) {
-			MapSqlParameterSource p = new MapSqlParameterSource();
-			p.addValue("name", mp3.getName());
-			p.addValue("author", mp3.getAuthor());
+    @Override
+    public Map<String, Integer> getStat() {
+        String sql = "select author_name, count(*) as count from " + mp3View + " group by author_name";
 
-			params[i] = p;
-			i++;
-		}
+        return jdbcTemplate.query(sql, new ResultSetExtractor<Map<String, Integer>>() {
 
-		// SqlParameterSource[] batch =
-		// SqlParameterSourceUtils.createBatch(listMP3.toArray());
-		int[] updateCounts = jdbcTemplate.batchUpdate(sql, params);
-		return updateCounts.length;
-	}
+            public Map<String, Integer> extractData(ResultSet rs) throws SQLException {
+                Map<String, Integer> map = new TreeMap<>();
+                while (rs.next()) {
+                    String author = rs.getString("author_name");
+                    int count = rs.getInt("count");
+                    map.put(author, count);
+                }
+                return map;
+            }
 
-	@Override
-	public Map<String, Integer> getStat() {
-		String sql = "select author_name, count(*) as count from " + mp3View + " group by author_name";
+            ;
 
-		return jdbcTemplate.query(sql, new ResultSetExtractor<Map<String, Integer>>() {
+        });
 
-			public Map<String, Integer> extractData(ResultSet rs) throws SQLException {
-				Map<String, Integer> map = new TreeMap<>();
-				while (rs.next()) {
-					String author = rs.getString("author_name");
-					int count = rs.getInt("count");
-					map.put(author, count);
-				}
-				return map;
-			};
+    }
 
-		});
+    @Override
+    public void delete(int id) {
+        String sql = "delete from mp3 where id=:id";
 
-	}
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("id", id);
 
-	@Override
-	public void delete(int id) {
-		String sql = "delete from mp3 where id=:id";
+        jdbcTemplate.update(sql, params);
+    }
 
-		MapSqlParameterSource params = new MapSqlParameterSource();
-		params.addValue("id", id);
+    @Override
+    public void delete(MP3 mp3) {
+        delete(mp3.getId());
+    }
 
-		jdbcTemplate.update(sql, params);
-	}
+    @Override
+    public MP3 getMP3ByID(int id) {
+        String sql = "select * from " + mp3View + " where mp3_id=:mp3_id";
 
-	@Override
-	public void delete(MP3 mp3) {
-		delete(mp3.getId());
-	}
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("mp3_id", id);
 
-	@Override
-	public MP3 getMP3ByID(int id) {
-		String sql = "select * from " + mp3View + " where mp3_id=:mp3_id";
+        return jdbcTemplate.queryForObject(sql, params, new MP3RowMapper());
+    }
 
-		MapSqlParameterSource params = new MapSqlParameterSource();
-		params.addValue("mp3_id", id);
+    @Override
+    public List<MP3> getMP3ListByName(String mp3Name) {
+        String sql = "select * from " + mp3View + " where upper(mp3_name) like :mp3_name";
 
-		return jdbcTemplate.queryForObject(sql, params, new MP3RowMapper());
-	}
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("mp3_name", "%" + mp3Name.toUpperCase() + "%");
 
-	@Override
-	public List<MP3> getMP3ListByName(String mp3Name) {
-		String sql = "select * from " + mp3View + " where upper(mp3_name) like :mp3_name";
+        return jdbcTemplate.query(sql, params, new MP3RowMapper());
+    }
 
-		MapSqlParameterSource params = new MapSqlParameterSource();
-		params.addValue("mp3_name", "%" + mp3Name.toUpperCase() + "%");
+    @Override
+    public List<MP3> getMP3ListByAuthor(String author) {
+        String sql = "select * from " + mp3View + " where upper(author_name) like :author_name";
 
-		return jdbcTemplate.query(sql, params, new MP3RowMapper());
-	}
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("author_name", "%" + author.toUpperCase() + "%");
 
-	@Override
-	public List<MP3> getMP3ListByAuthor(String author) {
-		String sql = "select * from " + mp3View + " where upper(author_name) like :author_name";
+        return jdbcTemplate.query(sql, params, new MP3RowMapper());
+    }
 
-		MapSqlParameterSource params = new MapSqlParameterSource();
-		params.addValue("author_name", "%" + author.toUpperCase() + "%");
+    @Override
+    public int getMP3Count() {
+        String sql = "select count(*) from " + mp3Table;
+        return jdbcTemplate.getJdbcOperations().queryForObject(sql, Integer.class);
+    }
 
-		return jdbcTemplate.query(sql, params, new MP3RowMapper());
-	}
+    private static final class MP3RowMapper implements RowMapper<MP3> {
 
-	@Override
-	public int getMP3Count() {
-		String sql = "select count(*) from " + mp3Table;
-		return jdbcTemplate.getJdbcOperations().queryForObject(sql, Integer.class);
-	}
+        @Override
+        public MP3 mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Author author = new Author();
+            author.setId(rs.getInt("author_id"));
+            author.setName("author_name");
 
-	private static final class MP3RowMapper implements RowMapper<MP3> {
+            MP3 mp3 = new MP3();
+            mp3.setId(rs.getInt("mp3_id"));
+            mp3.setName(rs.getString("mp3_name"));
+            mp3.setAuthor(author);
+            return mp3;
+        }
 
-		@Override
-		public MP3 mapRow(ResultSet rs, int rowNum) throws SQLException {
-			Author author = new Author();
-			author.setId(rs.getInt("author_id"));
-			author.setName("author_name");
-
-			MP3 mp3 = new MP3();
-			mp3.setId(rs.getInt("mp3_id"));
-			mp3.setName(rs.getString("mp3_name"));
-			mp3.setAuthor(author);
-			return mp3;
-		}
-
-	}
+    }
 
 }
